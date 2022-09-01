08788ff1fb4ea07efb417201b479b2a4a758c9e8
GaliSW <gary850204@gmail.com>
2022-09-01 23:06:40 +0800

    add - test project

--- component_test/jest.config.js
+++ component_test/jest.config.js
@@ -1,11 +1,6 @@
 const config = {
     verbose: true,
     testMatch: ["**/(*.)test.js", "**/(*.)spec.js"],
-    collectCoverageFrom: [
-        "**/*.{js,jsx}",
-        "!**/node_modules/**",
-        "!**/vendor/**",
-    ],
 };
 
 module.exports = config;
